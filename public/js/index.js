8d8d8c7a5483749557d1c878e80cca388a355f71
Lesleyworden <70115227+Lesleyworden@users.noreply.github.com>
2021-01-24 19:20:45 -0800

    Create index.js

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+init();
+
+async function init() {
+  // if there are note any search parameters in the url
+  if (location.search.split("=")[1] === undefined) {
+    // get the last workout
+    const workout = await API.getLastWorkout();
+    // if a workout is retrieved, navigate to the page for that workout
+    if (workout) {
+      // location.search = "?id=" + workout._id;
+      // removed for now doesn't seem to have a point
+    } else {
+      document.querySelector("#continue-btn").classList.add("d-none")
+    }
+  }
+}
