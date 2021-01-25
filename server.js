1cc9f891c24bd22d843c62997a951dcbd11fdb74
Lesleyworden <70115227+Lesleyworden@users.noreply.github.com>
2021-01-24 19:38:27 -0800

    Update server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+// import packages
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+// set port and create app
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// logger and asset middleware
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// home routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
+// connect to database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+// routes
+app.use(require("./routes/"));
+
+// start server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
